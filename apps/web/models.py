0c72f695661acebe7a640c8e8899b2305ad60e58
David <davidzxxjjj2003@gmail.com>
2021-09-16 11:09:13 -0600

    Modified templates and models

--- apps/web/models.py
+++ apps/web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class veterinaria(models.Model):
+    pk_Veterinaria = models.AutoField(primary_key=True, null=False, blank=False)
+    ubicacion = models.TextField(null=False, blank=False)
+    correo = models.TextField(null=False, blank=False)
+    facebook = models.TextField(null=False, blank=False)
+    telefono = models.IntegerField(null=False, blank=False)
+
+class producto(models.Model):
+    pk_producto = models.AutoField(primary_key=True, null=False, blank=False)
+    nombre = models.TextField(null=False, blank=False)
+    descripcion = models.TextField(null=False, blank=False)
+    cantidad = models.IntegerField(null=False, blank=False)
+    precio = models.IntegerField(null=False, blank=False)
+
+class mascota(models.Model):
+    pk_mascota = models.AutoField(primary_key=True, null=False, blank=False)
+    nombre_mascota = models.TextField(null=False, blank=False)
+    raza = models.TextField(null=False, blank=False)
+    color = models.TextField(null=False, blank=False)
+    edad = models.IntegerField(null=False, blank=False)
